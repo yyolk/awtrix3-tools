159d8e14da4f9225ff20024df6b384102fff2b53
Joseph Chiocchi <input.output@gmail.com>
2024-07-04 18:23:57 +0000

    add base64 encode fun

--- awtrix_files/la_icon.py
+++ awtrix_files/la_icon.py
@@ -1,3 +1,8 @@
+import base64
+
+from io import BytesIO
+
+from PIL import Image
 from uritemplate import URITemplate
 
 from awtrix_files.fetch import get_filename_and_bytesio
@@ -13,6 +18,18 @@ def get_lametric_icon(icon: int) -> (str, "BytesIO"):
     return get_filename_and_bytesio(url)
 
 
+def bytesio_image_to_base64(bo: BytesIO, gif_seek=1) -> str:
+    bo.seek(0)
+    pil_im = Image.open(bo)
+    if pil_im.format == "GIF":
+        pil_im.seek(pil_im.tell() + gif_seek)
+    pil_im = pil_im.convert("RGB")
+    newb = BytesIO()
+    pil_im.save(newb, "jpeg")
+    im_bytes = newb.getvalue()
+    return base64.b64encode(im_bytes).decode()
+
 if __name__ == "__main__":
-    filename, _ = get_lametric_icon(66)
+    filename, bo = get_lametric_icon(66)
     print(f"{filename=}")
+    print(bytesio_image_to_base64(bo))
