397e2c5b60de199db29f8dc60ea0035b86aaceb2
Joseph Chiocchi <input.output@gmail.com>
2024-07-06 23:32:48 +0000

    update list.py

--- awtrix_files/list.py
+++ awtrix_files/list.py
@@ -7,8 +7,12 @@ from uritemplate import URITemplate
 awtrix_uri_template = URITemplate("http://{awtrix_host}{/path=list}{?dir}")
 
 
-def list_icons(host, dir_="/ICONS") -> list[dict[str, str]]:
+def _list(host, dir_) -> list[dict[str, str]]:
     with urllib.request.urlopen(
         awtrix_uri_template.expand(awtrix_host=host, dir=dir_)
     ) as resp:
         return json.load(resp)
+
+
+def list_icons(host, dir_="/ICONS") -> list[dict[str, str]]:
+    return _list(host, dir_)
