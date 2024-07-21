3828a6b375d5a8e9d658c37d09af930f21c78e49
Joseph Chiocchi <input.output@gmail.com>
2024-07-21 18:55:18 +0000

    update sync.py
    - spike difference_icon_list for comparing the results of two set[Icon] from hosts to be used to queue up all the download/upload logic for syncing

--- awtrix_files/sync.py
+++ awtrix_files/sync.py
@@ -1,1 +1,17 @@
 """Holds syncing the files of one awtrix3 to another awtrix3."""
+
+from awtrix_files.list import Icon, list_icons
+
+
+def difference_icon_list(host1: str, host2: str) -> set[Icon]:
+    """Get the difference of the two set[Icon] of two hosts.
+
+    This will only return the larger set[Icon]. Where if one set[Icon]
+    is in the larger set[Icon], the difference comes down to which ones
+    aren't yet synced to the smaller set.
+    TODO:
+        - A better implementation that does a more comprehensive compare.
+    """
+    host1_icons = list_icons(host1)
+    host2_icons = list_icons(host2)
+    return max([host1_icons - host2_icons, host2_icons - host1_icons])
