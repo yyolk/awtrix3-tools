88680af0219b2e25e55a1f5d5a66344c84967450
Joseph Chiocchi <input.output@gmail.com>
2024-12-20 07:17:29 +0000

    update fetch.py, add docstrings

--- awtrix_files/fetch.py
+++ awtrix_files/fetch.py
@@ -8,7 +8,16 @@ from typing import Iterator, Union
 
 def download_file_in_chunks(
     url: str, chunk_size: int = 1024
-) -> "Iterator[Union[dict, bytes, ...]]":  # type: ignore
+) -> Iterator[Union[dict[str], bytes]]:
+    """Download a file with HTTP GET given a URL in chunks.
+
+    Args:
+        url: The URL to the resource to download.
+        chunk_size: The amount of bytes per chunk to return. Defaults to 1024.
+
+    Yields:
+        Always yields the headers first, then all subsequent yields are the chunks of the resource.
+    """    
     with urllib.request.urlopen(url) as response:
         # Yield the headers first
         yield response.headers
@@ -22,6 +31,12 @@ def download_file_in_chunks(
 
 
 def save_bytesio_to_file(bo: BytesIO, filename: str):
+    """Save a BytesIO to a filename.
+
+    Args:
+        bo: BytesIO (fp-like) to write.
+        filename: The filename to write to, can also be a path.
+    """    
     # Rewind the tape
     bo.seek(0)
     # Write the file to filename
@@ -30,6 +45,15 @@ def save_bytesio_to_file(bo: BytesIO, filename: str):
 
 
 def get_filename_and_bytesio(url) -> tuple[str, BytesIO]:
+    """Fetch and return the filename and the resource's tape.
+
+    Args:
+        url: The URL to fetch.
+
+    Returns:
+        str: The filename of the resource that was fetched (extracted from the URL).
+        BytesIO: The contents of the file in a seekable format.
+    """    
     bo = BytesIO()
     headers, *chunker = download_file_in_chunks(url)
     filetype = mimetypes.guess_extension(headers["content-type"])
